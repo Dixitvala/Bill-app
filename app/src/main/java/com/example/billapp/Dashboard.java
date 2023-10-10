7469fb94d69b6ad7f2a5afae9a7224f5b75f3a8b
user <valadixit2@gmail.com>
2023-10-10 19:05:38 +0530

    tt

--- app/src/main/java/com/example/billapp/Dashboard.java
+++ app/src/main/java/com/example/billapp/Dashboard.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -16,7 +17,7 @@ import android.widget.Toast;
 import com.google.android.material.navigation.NavigationView;
 
 public class Dashboard extends AppCompatActivity {
-//added drawer
+    //added drawer
     Button logout;
     TextView username;
     SharedPreferences sharedPreferences;
@@ -55,58 +56,45 @@ public class Dashboard extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 int id = item.getItemId();
-                if (id == R.id.dashboard)
-                {
+                if (id == R.id.dashboard) {
                     Toast.makeText(Dashboard.this, "Dashboard", Toast.LENGTH_SHORT).show();
-                }
-                else if (id == R.id.custVend)
-                {
-                    Toast.makeText(Dashboard.this, "Customer / Vendor", Toast.LENGTH_SHORT).show();
-                }
-                else if (id == R.id.product)
-                {
+                } else if (id == R.id.custVend) {
+                    startActivity(new Intent(getApplicationContext(), CustomerVendor.class));
+                } else if (id == R.id.product) {
                     Toast.makeText(Dashboard.this, "Product", Toast.LENGTH_SHORT).show();
-                }
-                else if (id == R.id.saleInvoice)
-                {
+                } else if (id == R.id.saleInvoice) {
                     Toast.makeText(Dashboard.this, "Sale Invoice", Toast.LENGTH_SHORT).show();
-                }
-                else if (id == R.id.purchaseInvoice)
-                {
+                } else if (id == R.id.purchaseInvoice) {
                     Toast.makeText(Dashboard.this, "Purchase Invoice", Toast.LENGTH_SHORT).show();
-                }
-                else if (id == R.id.inwardP)
-                {
+                } else if (id == R.id.inwardP) {
                     Toast.makeText(Dashboard.this, "Inward Payment", Toast.LENGTH_SHORT).show();
-                }
-                else if (id == R.id.outwardP)
-                {
+                } else if (id == R.id.outwardP) {
                     Toast.makeText(Dashboard.this, "Outward Payment", Toast.LENGTH_SHORT).show();
-                }
-                else if (id == R.id.quotation)
-                {
+                } else if (id == R.id.quotation) {
                     Toast.makeText(Dashboard.this, "Quotation", Toast.LENGTH_SHORT).show();
-                }
-                else if (id == R.id.proformaInvoice)
-                {
+                } else if (id == R.id.proformaInvoice) {
                     Toast.makeText(Dashboard.this, "Proforma Invoice", Toast.LENGTH_SHORT).show();
+                } else if (id == R.id.creditNote) {
+                    Toast.makeText(Dashboard.this, "Credit Note", Toast.LENGTH_SHORT).show();
+                } else if (id == R.id.debitNote) {
+                    Toast.makeText(Dashboard.this, "Debit Note", Toast.LENGTH_SHORT).show();
                 }
                 return false;
             }
-    });
+        });
 
-    // sharedPreference
-    sharedPreferences =
+        // sharedPreference
+        sharedPreferences =
 
-    getSharedPreferences("sharedData",MODE_PRIVATE);
+                getSharedPreferences("sharedData", MODE_PRIVATE);
 
-    editor =sharedPreferences.edit();
+        editor = sharedPreferences.edit();
 
-    String userName = sharedPreferences.getString("sharedEmail", "");
+        String userName = sharedPreferences.getString("sharedEmail", "");
 
 //        username.setText(userName);
 
-    // logout button for logout user from app
+        // logout button for logout user from app
 //        logout.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
@@ -116,7 +104,7 @@ public class Dashboard extends AppCompatActivity {
 //                finish();
 //            }
 //        });
-}
+    }
 
     @Override
     public void onBackPressed() {
